083989caa86ecd0da12491c27db7d22ad62867ba
Leonardo Omaña <leotangram@gmail.com>
2021-06-18 15:23:22 -0500

    Add space to icon

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react'
+import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Text } from 'react-native'
 import Tab1Screen from '../screens/Tab1Screen'
 import Tab2Screen from '../screens/Tab2Screen'
 import StackNavigator from './StackNavigator'
@@ -15,17 +16,47 @@ const Tabs = () => {
       }}
       tabBarOptions={{
         activeTintColor: colors.primary,
+        labelStyle: {
+          fontSize: 15
+        },
         style: {
           borderTopColor: colors.primary,
           borderTopWidth: 0,
           elevation: 0
         }
       }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, focused, size }) => {
+          let iconName: string = '1'
+          switch (route.name) {
+            case 'Tab1Screen':
+              iconName = 'T1'
+              break
+            case 'Tab2Screen':
+              iconName = 'T2'
+              break
+            case 'StackNavigator':
+              iconName = 'T3'
+              break
+          }
+          return <Text style={{ color }}>{iconName}</Text>
+        }
+      })}
     >
+      {/* <Tab.Screen
+        name="Tab1Screen"
+        component={Tab1Screen}
+        options={{
+          title: 'Tab1',
+          tabBarIcon: props => <Text style={{ color: props.color }}>T1</Text>
+        }}
+      /> */}
       <Tab.Screen
         name="Tab1Screen"
         component={Tab1Screen}
-        options={{ title: 'Tab1' }}
+        options={{
+          title: 'Tab1'
+        }}
       />
       <Tab.Screen
         name="Tab2Screen"
