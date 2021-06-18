756fb6d7cd53fa848fd52f7d682ba6394315e893
Leonardo Omaña <leotangram@gmail.com>
2021-06-18 16:35:52 -0500

    Install icons in iOS

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { View, Text } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Icon from 'react-native-vector-icons/Ionicons'
-import { styles } from '../theme/appTheme'
+import { styles, colors } from '../theme/appTheme'
 
 const Tab1Screen = () => {
+  const { top } = useSafeAreaInsets()
+
   useEffect(() => {
     console.log('Tab1Screen')
   }, [])
 
   return (
-    <View style={styles.globalMargin}>
+    <View style={{ ...styles.globalMargin, marginTop: top + 20 }}>
       <Text style={styles.title}>Icons</Text>
       <Text>
-        <Icon name="airplane-outline" size={80} color="#900" />
+        <Icon name="airplane-outline" size={80} color={colors.primary} />
+        <Icon name="aperture-outline" size={80} color={colors.primary} />
+        <Icon name="bandage-outline" size={80} color={colors.primary} />
+        <Icon name="balloon-outline" size={80} color={colors.primary} />
       </Text>
     </View>
   )
