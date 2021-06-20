92bde7d9c87b81f6137a3f17a4e546e9c882979b
Leonardo Omaña <leotangram@gmail.com>
2021-06-19 23:55:14 -0500

    Dispatch signIn

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import { Button, Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
+import { AuthContext } from '../context/AuthContext'
 
 const ContactsScreen = () => {
+  const {
+    authState: { isLoggedIn },
+    signIn
+  } = useContext(AuthContext)
+
   return (
-    <View>
-      <Text>ContactsScreen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>ContactsScreen</Text>
+      {!isLoggedIn && <Button title="Sign in" onPress={signIn} />}
     </View>
   )
 }
